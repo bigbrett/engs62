beb8ec6255bac156cbbbbad1a55211d39d73b993
Brett: ARMDEV <brett.r.nicholas.15@dartmouth.edu>
2015-01-14 12:06:20 -0500

    final (hopefully) solarized setup

--- .configs/startup.sh
+++ .configs/startup.sh
@@ -5,8 +5,10 @@
 
 cd ~/workspace
 
+
 # ensure git is up to date
 git status
+
 
 # Prompt user to pull from remote git repo
 read -p "Would you like to pull from remote git repository? (y/n) > " -n 1 
@@ -14,6 +16,7 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then 
   git pull origin master 
 fi 
+
 
 # Install Vim because Vi sucks
 echo; echo; echo "Checking for Vim...(sudo password: armdev"; echo; echo
@@ -26,12 +29,19 @@ if test -f "/home/armdev/workspace/.configs/.vimrc"; then {
   echo "ERROR... Could not locate ~/workspace/.configs/.vimrc"
 fi	
 
+
 # setup solarized
 echo; echo "Setting up Solarized Color Scheme...."; echo; echo
 bash ~/workspace/.configs/gnome-terminal-colors-solarized/install.sh
 eval `dircolors ~/workspace/.configs/dircolors-solarized/dircolors.256dark` 
-mkdir ~/.vim/colors
-mv ~/workspace/.configs/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
+
+if test -f "~/.vim/colors/solarized.vim"; then {
+	echo "colors updated"
+}; else {
+  mkdir ~/.vim/colors
+  mv ~/workspace/.configs/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
+}; 
+fi
 
 
 echo; echo "Dev environment restored....happy hacking!!"
