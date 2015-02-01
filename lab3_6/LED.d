4a3ec942c8bdd56f39f0cf8b47c58526e5f15eff
Brett: ARMDEV <brett.r.nicholas.15@dartmouth.edu>
2015-02-01 13:28:48 -0500

    finished with part 6

--- lab3_6/LED.d
+++
@@ -1,7 +0,0 @@
-LED.o: LED.c LED.h stdint.h stm32f4xx.h
-
-LED.h:
-
-stdint.h:
-
-stm32f4xx.h:
--- lab3_6/systick.d
+++
@@ -1,7 +0,0 @@
-systick.o: systick.c stdint.h stm32f4xx.h LED.h
-
-stdint.h:
-
-stm32f4xx.h:
-
-LED.h:
